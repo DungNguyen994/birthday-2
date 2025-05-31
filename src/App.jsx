f9bdf9909508b377a8807e2e4cdc1342c22bae6f
DungNguyen994 <worknoplay994@gmail.com>
2025-05-31 09:47:22 +0700

    use vite

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import AudioPlayer from "./AudioPlayer";
+
+function App() {
+  return (
+    <>
+      <AudioPlayer src="/music.mp3" title="Cute Audio Player" />
+    </>
+  );
+}
+
+export default App;
